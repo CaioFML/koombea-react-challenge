15963b6101fcf0fee8a0346f2687ac5e1df57255
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-06-14 20:14:33 -0300

    Add sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import './Sidebar.css';
+import TaskForm from './TaskForm';
+
+function Sidebar() {
+  return(
+    <div className="sidebar-background">
+      <TaskForm />
+      <></>
+      <></>
+    </div>
+  )
+}
+
+export default Sidebar
