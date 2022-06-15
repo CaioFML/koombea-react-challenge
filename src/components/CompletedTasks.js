76291de6d5d1ba3a09c8b3bdc8e8a691fac52432
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-06-15 18:15:03 -0300

    Add the completed tasks

---
+++ src/components/CompletedTasks.js
@@ -0,0 +1,12 @@
+import './CompletedTasks.css';
+
+function CompletedTasks() {
+  return(
+    <>
+      <p className='completed-tasks-label'>Completed tasks</p>
+      <p className='completed-tasks'>1</p>
+    </>
+  )
+}
+
+export default CompletedTasks
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,5 @@
+import CompletedTasks from './CompletedTasks';
+import RemainingTasks from './RemainingTasks';
 import './Sidebar.css';
 import TaskForm from './TaskForm';
 
@@ -5,8 +7,8 @@ function Sidebar() {
   return(
     <div className="sidebar-background">
       <TaskForm />
-      <></>
-      <></>
+      <RemainingTasks />
+      <CompletedTasks />
     </div>
   )
 }
