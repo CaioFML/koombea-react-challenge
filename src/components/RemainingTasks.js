0194d20eefd1eb46acac01ba80dc2b1c7dd3749f
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-06-15 18:12:17 -0300

    Add Remaining tasks

---
+++ src/components/RemainingTasks.js
@@ -0,0 +1,12 @@
+import './RemainingTasks.css';
+
+function RemainingTasks() {
+  return(
+    <>
+      <p className='remaining-tasks-label'>Remaining tasks</p>
+      <p className='remaining-tasks'>2</p>
+    </>
+  )
+}
+
+export default RemainingTasks
