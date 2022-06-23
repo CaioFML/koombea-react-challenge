5ea2f1bcb28452dcb8180e1bc94ca4f831d18a16
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-06-23 20:47:20 -0300

    Add button to change the task to complete

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,22 +1,44 @@
+import { useState } from 'react'
 import './Tasks.css'
 
 function Tasks() {
+  const [tasks, setTasks] = useState(
+    [
+      { key: '1', description: 'Label SAHUHUF ASUHFji ISJFIAFJASINAF', completed: false ,completedAt: null },
+      { key: '2', description: 'Label', completed: false, completedAt: null },
+      { key: '3', description: 'Lorem ipsum', completed: false, completedAt: null }
+    ]
+  )
+
+  const changeTaskToComplete = (task) => {
+    let taskId = task.key
+
+    setTasks(prevState => {
+      return (prevState.map(task => {
+        if (task.key === taskId) {
+          return {...task, completed: true }
+        }
+
+        return task
+      }))
+    })
+  }
+
   return(
     <div className='container'>
-      <div className='tasks'>
-        <p className='tasks-text'>Label SAHUHUF ASUHFji ISJFIAFJASINAF</p>
-        <button className='tasks-button'>Mark as complete</button>
-      </div>
-
-      <div className='tasks'>
-        <p className='tasks-text'>Label</p>
-        <button className='tasks-button'>Mark as complete</button>
-      </div>
-
-      <div className='tasks'>
-        <p className='tasks-text-completed'>Label ASHUFAS HUASFH HAUFHSUA FUSAF HU</p>
-        <button className='tasks-button-completed'>Completed 5s ago</button>
-      </div>
+      {console.log(tasks)}
+      {tasks.map(task => {
+        return(
+          <div className='tasks' key={task.key}>
+            <p className={task.completed ? 'tasks-text-completed' : 'tasks-text'}>{task.description}</p>
+            <button
+            className={task.completed ? 'tasks-button-completed' : 'tasks-button'}
+            onClick={() => changeTaskToComplete(task)}>
+              {task.completed ? 'Completed at ...' : 'Mark as complete'}
+            </button>
+          </div>
+        )
+      })}
     </div>
   )
 }
